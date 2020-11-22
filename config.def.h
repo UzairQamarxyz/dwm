f995f0549709a9ae02a0ebfa909b2c0b40d6f933
UzayrQamarxyz <uzayrqamarxyz@tutanota.com>
2020-11-23 00:37:16 +0500

    return of the siji

--- config.def.h
+++ config.def.h
@@ -1,32 +1,36 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappih    = 7;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 7;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 7;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 7;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka Aile:size=10:antialias=true" };
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
-static char termcol0[] = "#000000"; /* black   */
-static char termcol1[] = "#ff0000"; /* red     */
-static char termcol2[] = "#33ff00"; /* green   */
-static char termcol3[] = "#ff0099"; /* yellow  */
-static char termcol4[] = "#0066ff"; /* blue    */
-static char termcol5[] = "#cc00ff"; /* magenta */
-static char termcol6[] = "#00ffff"; /* cyan    */
-static char termcol7[] = "#d0d0d0"; /* white   */
-static char termcol8[]  = "#808080"; /* black   */
-static char termcol9[]  = "#ff0000"; /* red     */
+static const unsigned int borderpx = 2; /* border pixel of windows */
+static const unsigned int gappih = 7;   /* horiz inner gap between windows */
+static const unsigned int gappiv = 7;   /* vert inner gap between windows */
+static const unsigned int gappoh =
+    7; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov =
+    7; /* vert outer gap between windows and screen edge */
+static int smartgaps =
+    0; /* 1 means no outer gap when there is only one window */
+static const unsigned int snap = 32; /* snap pixel */
+static const int showbar = 1;        /* 0 means no bar */
+static const int topbar = 1;         /* 0 means bottom bar */
+static const char *fonts[] = {"Barlow:size=12:antialias=true",
+                              "Siji:size=14:antialias=true:autohint=true"};
+static char normbgcolor[] = "#222222";
+static char normbordercolor[] = "#444444";
+static char normfgcolor[] = "#bbbbbb";
+static char selfgcolor[] = "#eeeeee";
+static char selbordercolor[] = "#005577";
+static char selbgcolor[] = "#005577";
+static char termcol0[] = "#000000";  /* black   */
+static char termcol1[] = "#ff0000";  /* red     */
+static char termcol2[] = "#33ff00";  /* green   */
+static char termcol3[] = "#ff0099";  /* yellow  */
+static char termcol4[] = "#0066ff";  /* blue    */
+static char termcol5[] = "#cc00ff";  /* magenta */
+static char termcol6[] = "#00ffff";  /* cyan    */
+static char termcol7[] = "#d0d0d0";  /* white   */
+static char termcol8[] = "#808080";  /* black   */
+static char termcol9[] = "#ff0000";  /* red     */
 static char termcol10[] = "#33ff00"; /* green   */
 static char termcol11[] = "#ff0099"; /* yellow  */
 static char termcol12[] = "#0066ff"; /* blue    */
@@ -34,27 +38,14 @@ static char termcol13[] = "#cc00ff"; /* magenta */
 static char termcol14[] = "#00ffff"; /* cyan    */
 static char termcol15[] = "#ffffff"; /* white   */
 static char *termcolor[] = {
-  termcol0,
-  termcol1,
-  termcol2,
-  termcol3,
-  termcol4,
-  termcol5,
-  termcol6,
-  termcol7,
-  termcol8,
-  termcol9,
-  termcol10,
-  termcol11,
-  termcol12,
-  termcol13,
-  termcol14,
-  termcol15,
+    termcol0,  termcol1,  termcol2,  termcol3,  termcol4,  termcol5,
+    termcol6,  termcol7,  termcol8,  termcol9,  termcol10, termcol11,
+    termcol12, termcol13, termcol14, termcol15,
 };
 static char *colors[][3] = {
-       /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+    /*               fg           bg           border   */
+    [SchemeNorm] = {normfgcolor, normbgcolor, normbordercolor},
+    [SchemeSel] = {selfgcolor, selbgcolor, selbordercolor},
 };
 
 #define FORCE_VSPLIT                                                           \
@@ -82,9 +73,9 @@ static const int resizehints =
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    {"󰙀", tile}, /* first entry is default */
-    {"󰹙", NULL}, /* no layout function means floating behavior */
-    {"󰝤", monocle}, {"|M|", centeredmaster}, {NULL, NULL},
+    {"", tile}, /* first entry is default */
+    {"", NULL}, /* no layout function means floating behavior */
+    {"", monocle}, {"|M|", centeredmaster}, {NULL, NULL},
 };
 
 /* key definitions */
@@ -107,9 +98,9 @@ static char dmenumon[2] =
 static const char *dmenucmd[] = {"dmenu_run", "-m",  dmenumon,    "-nb",
                                  normbgcolor, "-nf", normfgcolor, "-sb",
                                  selbgcolor,  "-sf", selfgcolor,  NULL};
-static const char *termcmd[] = {"termite", NULL};
-static const char *rangercmd[] = {"termite", "-e", "ranger", NULL};
-static const char *cmuscmd[] = {"termite", "-e", "cmus", NULL};
+static const char *termcmd[] = {"alacritty", NULL};
+static const char *rangercmd[] = {"alacritty", "-e", "ranger", NULL};
+static const char *cmuscmd[] = {"alacritty", "-e", "cmus", NULL};
 
 #include "shiftview.c"
 #include <X11/XF86keysym.h>
@@ -158,7 +149,7 @@ static Key keys[] = {
     {MODKEY, XK_F7, spawn, SHCMD("~/.local/bin/displayselect")},
     {MODKEY, XK_BackSpace, spawn, SHCMD("~/.local/bin/sysact")},
     {MODKEY, XK_w, spawn, SHCMD("brave")},
-    {MODKEY | ShiftMask, XK_w, spawn, SHCMD("termite -e nmtui")},
+    {MODKEY | ShiftMask, XK_w, spawn, SHCMD("alacritty -e nmtui")},
     {MODKEY, XK_x, spawn, SHCMD("~/.local/bin/i3lock/lock.sh")},
     {MODKEY, XK_p, spawn,
      SHCMD("cmus-remote --pause && pkill -RTMIN+20 dwmblocks")},
