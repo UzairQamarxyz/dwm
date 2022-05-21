9082999297b15fbdfcb6d783ad621cfc6d4516f6
UzairQamarxyz <uzairq@xgrid.co>
2022-05-21 15:04:17 +0500

    Using vanitygaps instead of fullgaps

--- config.def.h
+++ config.def.h
@@ -2,12 +2,16 @@
 
 /* appearance */
 static const unsigned int borderpx = 2; /* border pixel of windows */
-static const unsigned int gappx = 10;   /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int swallowfloating =
     0;                        /* 1 means swallow floating windows by default */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
+static const unsigned int gappih = 15; /* horiz inner gap between windows */
+static const unsigned int gappiv = 15; /* vert inner gap between windows */
+static const unsigned int gappoh = 15; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov = 15; /* vert outer gap between windows and screen edge */
+static int smartgaps = 0; /* 1 means no outer gap when there is only one window */
 static const int horizpadbar = 2; /* horizontal padding for statusbar */
 static const int vertpadbar = 5;  /* vertical padding for statusbar */
 static const char *fonts[] = {
@@ -72,11 +76,17 @@ static const int resizehints =
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
     /* symbol     arrange function */
     {"[]=", tile}, /* first entry is default */
     {"><>", NULL}, /* no layout function means floating behavior */
     {"[M]", monocle},
+    {"H[]", deck },
+    {"|M|", centeredmaster },
+    { NULL, NULL },
 };
 
 /* key definitions */
@@ -128,6 +138,8 @@ static Key keys[] = {
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
     {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
+    {MODKEY | ShiftMask, XK_d, setlayout, {.v = &layouts[3]}},
+    {MODKEY | ShiftMask, XK_m, setlayout, {.v = &layouts[4]}},
     {MODKEY, XK_Page_Down, viewnext, {.i = +1}},
     {MODKEY, XK_Page_Up, viewprev, {.i = -1}},
     {MODKEY, XK_space, setlayout, {0}},
