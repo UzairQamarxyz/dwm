385bd461fcb14df3b4955d1f1c2461e64e2cf956
UzairQamarxyz <uzairq@xgrid.co>
2023-03-05 16:44:44 +0500

    Adding statuspadding
    Signed-off-by: UzairQamarxyz <uzairq@xgrid.co>

--- config.def.h
+++ config.def.h
@@ -2,14 +2,16 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int gappx     = 10;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 5;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 5;        /* vertical padding for statusbar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "Iosevka Custom:size=14" };
-static const char dmenufont[]       = "Iosevka Custom:size=14";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -57,7 +59,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"", "", "", "", "", "", ""};
+static const char *tags[] = {" ", " ", " ", " ", " ", " ", ""};
 
 static const Rule rules[] = {
     /* xprop(1):
