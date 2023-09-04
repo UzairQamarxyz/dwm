0be1d895ebda83d6f64a9d00555125308504d01e
UzairQamarxyz <uzairq@xgrid.co>
2023-09-04 23:21:18 +0500

    status2d
    Signed-off-by: UzairQamarxyz <uzairq@xgrid.co>

--- config.def.h
+++ config.def.h
@@ -2,25 +2,26 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 14;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=12" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static char *fonts[]          = { "IosevkaCustom Nerd Font:size=12" };
+static char dmenufont[]       = "IosevkaCustom Nerd Font:size=12";
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3]      = {
     /*               fg         bg         border   */
-    [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+    [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
-static const char *tags[] = {"", " ", "󰉋", "󰝚", "󰎁", "󰈙", ""};
+static const char *tags[] = {" ", "󰈹 ", "󰉋 ", "󰝚 ", "󰎁 ", "󰈙 ", "󰒱 "};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -70,6 +71,20 @@ static const char *lockscreen[]  = { "betterlockscreen", "--lock", NULL };
 static const char *screenshot[]  = { "flameshot", "gui", NULL };
 static const char *brightnessup[]  = { "brightnessctl", "set", "10%+", NULL };
 static const char *brightnessdown[]  = { "brightnessctl", "set", "10%-", NULL };
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "fonts",              STRING,  &fonts },
+		{ "dmenufont",          STRING,  &dmenufont },
+		{ "normbgcolor",        STRING,  &normbgcolor },
+		{ "normbordercolor",    STRING,  &normbordercolor },
+		{ "normfgcolor",        STRING,  &normfgcolor },
+		{ "selbgcolor",         STRING,  &selbgcolor },
+		{ "selbordercolor",     STRING,  &selbordercolor },
+		{ "selfgcolor",         STRING,  &selfgcolor },
+};
 
 #include <X11/XF86keysym.h>
 
