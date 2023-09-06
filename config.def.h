96d17b3c9ac506c655154d065f379de482400c78
UzairQamarxyz <uzairq@xgrid.co>
2023-09-06 12:55:00 +0500

    xrdb
    Signed-off-by: UzairQamarxyz <uzairq@xgrid.co>

--- config.def.h
+++ config.def.h
@@ -8,13 +8,46 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "IosevkaCustom Nerd Font:size=12" };
 static const char dmenufont[]       = "IosevkaCustom Nerd Font:size=12";
-static const char termcol0[] =
-static const char normbgcolor[]           = "#222222";
-static const char normbordercolor[]       = "#444444";
-static const char normfgcolor[]           = "#bbbbbb";
-static const char selfgcolor[]            = "#eeeeee";
-static const char selbordercolor[]        = "#005577";
-static const char selbgcolor[]            = "#005577";
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char termcol0[] = "#000000"; /* black   */
+static char termcol1[] = "#ff0000"; /* red     */
+static char termcol2[] = "#33ff00"; /* green   */
+static char termcol3[] = "#ff0099"; /* yellow  */
+static char termcol4[] = "#0066ff"; /* blue    */
+static char termcol5[] = "#cc00ff"; /* magenta */
+static char termcol6[] = "#00ffff"; /* cyan    */
+static char termcol7[] = "#d0d0d0"; /* white   */
+static char termcol8[]  = "#808080"; /* black   */
+static char termcol9[]  = "#ff0000"; /* red     */
+static char termcol10[] = "#33ff00"; /* green   */
+static char termcol11[] = "#ff0099"; /* yellow  */
+static char termcol12[] = "#0066ff"; /* blue    */
+static char termcol13[] = "#cc00ff"; /* magenta */
+static char termcol14[] = "#00ffff"; /* cyan    */
+static char termcol15[] = "#ffffff"; /* white   */
+static char *termcolor[] = {
+  termcol0,
+  termcol1,
+  termcol2,
+  termcol3,
+  termcol4,
+  termcol5,
+  termcol6,
+  termcol7,
+  termcol8,
+  termcol9,
+  termcol10,
+  termcol11,
+  termcol12,
+  termcol13,
+  termcol14,
+  termcol15,
+};
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
