40446c31516fe91f8ac2ac12cbe84d61d68b92c1
UzairQamarxyz <uzairqamarxyz@gmail.com>
2025-04-28 16:32:39 +0500

    barpadding
    Signed-off-by: UzairQamarxyz <uzairqamarxyz@gmail.com>

--- config.def.h
+++ config.def.h
@@ -1,10 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = {"siji:size=11", "IosevkaCustom Nerd Font:size=11"};
 static const char dmenufont[]       = "IosevkaCustom Nerd Font:size=11";
 static const char norm_bg[]         = "#1F1F28";
